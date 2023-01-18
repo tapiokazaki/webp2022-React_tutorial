b5fa8624c51199cc08631fd5cf77cbbe2d6af169
tapiokazaki <114901262+tapiokazaki@users.noreply.github.com>
2023-01-18 18:32:23 +0900

    Delete src directory

--- src/api.js
+++
@@ -1,7 +0,0 @@
-export async function fetchImages(amiibo){
-    const response = await fetch(
-        `https://www.amiiboapi.com/api/amiibo/?name=${amiibo}`
-    );
-    const data = await response.json();
-    return data.amiibo;
-}
